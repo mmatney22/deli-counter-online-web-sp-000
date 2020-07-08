3af701e1791b093d1e43b0242eb9ecd871789bdc
Morgan Matney <morgan.matney89@gmail.com>
2020-07-08 13:41:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   else
     current_line = "The line is currently:"
     katz_deli.each_with_index do |name, i|
-      puts "#{current_line} #{index + 1}. #{name}"
+      puts "#{current_line} #{i + 1}. #{name}"
     end
   end
 end
