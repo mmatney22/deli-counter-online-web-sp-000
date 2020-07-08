48a06cf014abf49f536f42611b8f09106cb6c9ea
Morgan Matney <morgan.matney89@gmail.com>
2020-07-08 13:39:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
-  else 
+  else
     katz_deli.each_with_index do |name, i|
       puts "The line is currently: #{index + 1}. #{name}"
   end
